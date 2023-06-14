b4428f7c78039a2bb75f4fe904ee7573c34891ff
Granit-Developer <nitist2@gmail.com>
2023-06-14 04:16:42 +0200

    first commit

---
+++ admin/assets/inc/footer.php
@@ -0,0 +1,4 @@
+<div class="splash-footer">
+    &copy; 2023 - <?php echo date ('Y');?>
+    Flix Train Online Reservation System | Developed By Granit Statovci
+</div>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Flix Train Online Reservation System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Flix Train Online Reservation System</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>Online Flix Train Reservation System</h2>
+							<p>Best, Simple, and User Friendly Way To Reserve Train Tickets Effectively</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Get in touch</h2>
+							
+							<ul class="contact">
+								<li class="icon solid fa-home">
+									Online Train Reservation System<br />
+									PHP Project<br />
+									
+								</li>
+								<li class="icon solid fa-phone">(+383) 44 100 200</li>
+								<li class="icon solid fa-envelope"><a href="#">mail@flixtrain.com</a></li>
+								<li class="icon brands fa-twitter"><a href="#">twitter.com/flixtrain</a></li>
+								<li class="icon brands fa-facebook-f"><a href="#">facebook.com/flixtrain</a></li>
+								<li class="icon brands fa-instagram"><a href="#">instagram.com/flixtrain</a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; Flix Train Reservation.</li><li>Developed By: Granit Statovci</li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
